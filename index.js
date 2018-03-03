711d98d5fb9867cc49a0215c7351f849824a4c84
Pedro Palhari Santos <pedropalhari@gmail.com>
2018-03-03 11:44:09 -0300

    commit inicial

---
+++ index.js
@@ -0,0 +1,123 @@
+var globalLogic = [];
+
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+(function q(){
+  rl.question("> ", answer => {
+    logicAnalyzer(answer);
+		q();
+	});
+})();
+
+function logicGen(num, array = []) {
+  if (num == 0) {
+    globalLogic.push(array);
+    return;
+  }
+
+  --num;
+
+  if (array.length == 0) {
+    logicGen(num, [0]);
+    logicGen(num, [1]);
+  } else {
+    logicGen(num, array.concat([0]));
+    logicGen(num, array.concat([1]));
+  }
+}
+
+function stringParser(string = "") {
+  string = string.toUpperCase();
+
+  while (string.indexOf(".") != -1) string = string.replace(".", "&&");
+
+  while (string.indexOf("+") != -1) string = string.replace("+", "||");
+
+  while (string.indexOf("~") != -1) string = string.replace("~", "!");
+
+  while (string.indexOf(" ") != -1) string = string.replace(" ", "");
+
+  return string;
+}
+
+function stringToVars(string = "") {
+  string = string.toUpperCase();
+  for (let i = 0; i < string.length; i++) {
+    if (string.charCodeAt(i) < 65 || string.charCodeAt(i) > 90) {
+      string = string.substring(0, i) + string.substring(i + 1);
+      i--;
+    }
+  }
+
+  while (string.indexOf(" ") != -1) string = string.replace(" ", "");
+
+  let stringVarArray = [];
+  string.split("").map(strVar => {
+    if (stringVarArray.indexOf(strVar) == -1) {
+      stringVarArray.push(strVar);
+    }
+  });
+
+  return stringVarArray;
+}
+
+function printLine(num) {
+  let string = "";
+
+  for (var i = 0; i < num; i++) string += "-";
+
+  console.log(string);
+}
+
+function printResults(logicString, logicVars, logicArrays, results) {
+  printLine(logicVars.length * 4 + logicString.length + 4);
+  let str = "|";
+  for (let i = 0; i < logicVars.length; i++) str += ` ${logicVars[i]} |`;
+  str += ` ${logicString} |`;
+  console.log(str);
+  printLine(logicVars.length * 4 + logicString.length + 4);
+
+  for (let j = 0; j < logicArrays.length; j++) {
+    str = "|";
+    for (let i = 0; i < logicVars.length; i++) str += ` ${logicArrays[j][i]} |`;
+
+    let str2 = "";
+    for (let k = 0; k < logicString.length - 1; k++) {
+      str2 += " ";
+    }
+
+    let index = Math.floor(logicString.length / 2);
+    str2 =
+      str2.substring(0, index) + (results[j] ? 1 : 0) + str2.substring(index);
+
+    str += ` ${str2} |`;
+
+    console.log(str);
+    printLine(logicVars.length * 4 + logicString.length + 4);
+  }
+}
+
+function logicAnalyzer(string = "") {
+  globalLogic = [];
+  let logicString = stringParser(string);
+  let logicVars = stringToVars(string);
+  logicGen(logicVars.length);
+  let logicArrays = globalLogic;
+  let results = [];
+
+  //console.log(logicString, logicVars, logicArrays);
+
+  logicArrays.map(arr => {
+    logicVars.map((logicVar, index) => {
+      eval(`${logicVar}=${arr[index]}`);
+    });
+    results.push(eval(logicString));
+  });
+
+  printResults(logicString, logicVars, logicArrays, results);
+}
